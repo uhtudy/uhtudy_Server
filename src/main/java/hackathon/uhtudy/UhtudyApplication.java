a7c259fa63ecc1bf56db0da2c091cfcc28350f96
leedongyeop <ldy_1204@naver.com>
2023-06-29 08:35:08 +0900

    initial commit

---
+++ src/main/java/hackathon/uhtudy/UhtudyApplication.java
@@ -0,0 +1,13 @@
+package hackathon.uhtudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UhtudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UhtudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackathon/uhtudy/UhtudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.uhtudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UhtudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
