d380f1d6cc184e548b24707efdb65a114e1d4e59
leedongyeop <ldy_1204@naver.com>
2023-06-29 19:04:21 +0900

    test : 과제 도메인 테스트 작성

---
+++ src/test/java/settings/ApiTest.java
@@ -0,0 +1,19 @@
+package settings;
+
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApiTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = port;
+    }
+}
---
+++ src/test/java/settings/ServiceTest.java
@@ -0,0 +1,9 @@
+package settings;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+public class ServiceTest {
+}
