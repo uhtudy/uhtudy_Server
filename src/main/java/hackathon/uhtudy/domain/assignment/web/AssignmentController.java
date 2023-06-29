320c80ecd6fa73220db84bab1551bfad24381782
leedongyeop <ldy_1204@naver.com>
2023-06-29 14:45:33 +0900

    fix : repository 내에 save 메소드 삭제

--- src/main/java/hackathon/uhtudy/domain/assignment/web/AssignmentController.java
+++ src/main/java/hackathon/uhtudy/domain/assignment/web/AssignmentController.java
@@ -2,12 +2,11 @@ package hackathon.uhtudy.domain.assignment.web;
 
 import hackathon.uhtudy.domain.assignment.application.AssignmentService;
 import hackathon.uhtudy.domain.assignment.persistence.Assignment;
+import hackathon.uhtudy.domain.assignment.web.request.SubmitAssignmentRequest;
 import hackathon.uhtudy.domain.assignment.web.response.AssignmentDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +17,20 @@ public class AssignmentController {
 
 
     @GetMapping("/{assignmentId}")
-    public AssignmentDto getCodeReview(@PathVariable final Long assignmentId) {
+    public ResponseEntity<AssignmentDto> getCodeReview(@PathVariable final Long assignmentId) {
 
         final Assignment codeReview = assignmentService.getCodeReview(assignmentId);
-        return new AssignmentDto(codeReview);
+        return ResponseEntity.ok(new AssignmentDto(codeReview));
+    }
+
+
+    @PostMapping("/studies/{studyId}/{weekNum}")
+    public ResponseEntity<?> submitAssignment(
+            @PathVariable final Long studyId,
+            @PathVariable final int weekNum,
+            @RequestBody final SubmitAssignmentRequest request) {
+
+        assignmentService.submitAssignment(studyId, weekNum, request);
+        return ResponseEntity.ok().build();
     }
 }
